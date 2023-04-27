8e3f81f27691238549bab78c98d01bf870392b04
ChrisEski <eschristos1@gmail.com>
2023-04-28 00:08:19 +0300

    homepage layout complete

---
+++ app/(home)/Tech.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import Card from '../(shared)/Card';
+
+type Props = {};
+
+const Tech = (props: Props) => {
+  return (
+    <section className="pt-3 pb-10">
+      <hr className="border-1" />
+      {/* HEADER */}
+      <div className="flex items-center gap-3 my-8">
+        <h4 className="bg-accent-orange py-2 px-5 text-wh-900 text-sm font-bold">
+          HOT
+        </h4>
+        <p className="font-bold text-2xl">Latest news in Technology</p>
+      </div>
+
+      {/* GRID */}
+      <div className="sm:grid grid-cols-2 grid-row-3 gap-x-8 gap-y-8 my-5">
+        {/* LARGE CARD */}
+        <Card
+          className="bg-wh-500 col-span-1 row-span-3"
+          imageHeight="h-96"
+          isLongForm></Card>
+        {/* SMALL CARDS */}
+        <Card
+          className="bg-wh-500 col-span-1 row-span-1 mt-10 sm:mt-0 flex justify-between gap-3"
+          imageHeight="h-48"
+          isSmallCard></Card>
+        <Card
+          className="bg-wh-500 col-span-1 row-span-1 mt-10 sm:mt-0 flex justify-between gap-3"
+          imageHeight="h-48"
+          isSmallCard></Card>
+        <Card
+          className="bg-wh-500 col-span-1 row-span-1 mt-10 sm:mt-0 flex justify-between gap-3"
+          imageHeight="h-48"
+          isSmallCard></Card>
+      </div>
+    </section>
+  );
+};
+
+export default Tech;
